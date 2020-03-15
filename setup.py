f3d584af9c165bfdfca9e06313172f07f4b10265
William Lynn <william.lynn.1337@gmail.com>
2020-03-15 00:03:10 +0000

    finished project

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
     long_description=readme,
     author='William',
     author_email='wiccawill420@gmail.com',
-    install_requires=[],
+    install_requires=['boto3'],
     packages=find_packages('src'),
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ]
+    }
 )
--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -10,13 +10,31 @@ class DriverAction(Action):
         namespace.driver = driver.lower()
         namespace.destination = destination
 
-
 def create_parser():
     parser = ArgumentParser()
     parser.add_argument('url', help="URL of the PostgreSQL database to backup")
-    parser.add_argument('--driver',
+    parser.add_argument('--driver', '-d',
             help="How and where to store backup",
             nargs=2,
             action=DriverAction,
+            metavar=('driver', 'destination'),
             required=True)
     return parser
+
+def main():
+    import boto3
+    import time
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing up to {args.destination} in S3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {args.destination}")
+        storage.local(dump.stdout, outfile)
